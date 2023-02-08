536aa1727e423098da208d1fc2d325a3c9407f25
hbckleikamp <49785660+hbckleikamp@users.noreply.github.com>
2023-02-08 09:45:54 +0100

    Update Download_parse_Unimod.py

--- Download_parse_Unimod.py
+++ Download_parse_Unimod.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb  7 11:15:26 2023
-
-@author: ZR48SA
-"""
-
 #%% change directory to script directory (should work on windows and mac)
 import os
 from pathlib import Path
@@ -48,13 +41,17 @@ for ix,mod in enumerate(mods):
 
     title=mod.split('title="')[1].split('"')[0]
     full_name=mod.split('full_name="')[1].split('"')[0]
-    delta_mass=mod.split('umod:delta mono_mass="')[1].split('"')[0]
-    composition=[mod.split('element symbol="'+e+'" number="')[-1].split('"')[0] for e in elements]
+    
+    d=mod.split('umod:delta mono_mass="')[1]
+    delta_mass=d.split('"')[0]
+    composition=[d.split('element symbol="'+e+'" number="')[1].split('"')[0] if 'element symbol="'+e+'" number="' in d else 0 for e in elements]
     parsed.append([title,full_name,delta_mass]+composition)
     
-parsed_df=pd.DataFrame(parsed,columns=["title","full_name","delta_mass"]+elements)
+parsed_df=pd.DataFrame(parsed,columns=["title","full_name","delta_mass"]+elements).set_index("title")
+parsed_df.loc["",:]=["0"]*len(parsed_df.columns) #add zero row
 parsed_df[elements]=parsed_df[elements].replace("title=","0").astype(int)
 parsed_df["delta_mass"]=parsed_df["delta_mass"].astype(float)
-parsed_df=parsed_df.set_index("title")
+
+
 
 parsed_df.to_csv("unimod_parsed.txt",sep="\t")
